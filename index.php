5f8ec0479c8b03905a705cf316d3161775f10bb9
Sm Tamjid <tamjidsm@gmail.com>
2014-10-16 21:25:23 -0400

    index html header error3

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-//#error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-//#ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+#error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+#ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 <?php require ('logic1.php') ?>
 
-<!DOCTYPE html>
+<!DOCTYPE html
 <html>	
 <?php include('header.php')?>
+
  <body class='text'>
 <?php include('content.php')?>
  </body>
