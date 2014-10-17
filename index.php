18d2af5cb3f9f676d3ce2a3ea8c4c2a954bc80fe
Sm Tamjid <tamjidsm@gmail.com>
2014-10-16 21:11:58 -0400

    index html header error1

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<!DOCTYPE html>
-<?php include('header.php')?>
+<?php
+//#error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+//#ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
 <?php require ('logic1.php') ?>
 
+<!DOCTYPE html>
+<?php include('header.php')?>
 <html>	
  <body class='text'>
 <?php include('content.php')?>
