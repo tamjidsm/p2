62fb0d4b2b191d31ede20981bf205706074ba4e6
Sm Tamjid <tamjidsm@gmail.com>
2014-10-16 20:53:14 -0400

    index error fix

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php
-//#error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-//#ini_set('display_errors', 1); # Display errors on page (instead of a log file)
-?>
+
 <?php require ('logic1.php') ?>
 
 <!DOCTYPE html
